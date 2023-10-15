da3b86f860a75e2691b6fed7f379458e88614b19
Daniel Saad <dsaad68@users.noreply.github.com>
2023-10-15 19:44:44 +0200

    🚀 Init

---
+++ custom/plugins.lua
@@ -0,0 +1,158 @@
+local cmp = require "cmp"
+
+local plugins = {
+  {
+    "rcarriga/nvim-dap-ui",
+    dependencies = "mfussenegger/nvim-dap",
+    config = function()
+      local dap = require("dap")
+      local dapui = require("dapui")
+      dapui.setup()
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function()
+        dapui.close()
+      end
+    end
+  },
+  {
+    "mfussenegger/nvim-dap",
+    init = function()
+      require("core.utils").load_mappings("dap")
+    end,
+    config = function(_, opts)
+      require("core.utils").load_mappings("dap")
+    end
+  },
+  {
+    "mfussenegger/nvim-dap-python",
+    ft = "python",
+    dependencies = {
+      "mfussenegger/nvim-dap",
+      "rcarriga/nvim-dap-ui",
+    },
+    config = function(_,opts)
+      -- for linux and mac:
+      -- local path = "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
+      local path = "$HOME/AppData/Local/nvim-data/mason/packages/debugpy/venv/Scripts/python.exe"
+      require("dap-python").setup(path)
+      require("core.utils").load_mappings("dap_python")
+    end,
+  },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    ft = {"python"},
+    opts = function ()
+      return require "custom.configs.null-ls"
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "debugpy",
+        "black",
+        "mypy",
+        "ruff",
+        "pyright",
+        "rust-analyzer",
+      },
+    },
+  },
+  {
+  "neovim/nvim-lspconfig",
+  config = function ()
+    require "plugins.configs.lspconfig"
+    require "custom.configs.lspconfig"
+  end
+  },
+  {
+    "simrat39/rust-tools.nvim",
+    ft = "rust",
+    dependencies = "neovim/nvim-lspconfig",
+    opts = function ()
+      return require "custom.configs.rust-tools"
+    end,
+    config = function(_, opts)
+      require('rust-tools').setup(opts)
+    end
+  },
+    {
+    'saecki/crates.nvim',
+    ft = {"toml"},
+    config = function(_, opts)
+      local crates  = require('crates')
+      crates.setup(opts)
+      require('cmp').setup.buffer({
+        sources = { { name = "crates" }}
+      })
+      crates.show()
+      require("core.utils").load_mappings("crates")
+    end,
+  },
+  {
+    "rust-lang/rust.vim",
+    ft = "rust",
+    init = function ()
+      vim.g.rustfmt_autosave = 1
+    end
+  },
+  {
+    "theHamsta/nvim-dap-virtual-text",
+    lazy = false,
+    config = function(_, opts)
+      require("nvim-dap-virtual-text").setup()
+    end
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function()
+      local M = require "plugins.configs.cmp"
+      M.completion.completeopt = "menu,menuone,noselect"
+      M.mapping["<CR>"] = cmp.mapping.confirm {
+        behavior = cmp.ConfirmBehavior.Insert,
+        select = false,
+      }
+      table.insert(M.sources, {name = "crates"})
+      return M
+    end,
+  },
+  {
+    "NvChad/nvterm",
+    config = function ()
+      require("nvterm").setup({
+        terminals = {
+          shell = vim.o.shell,
+          list = {},
+          type_opts = {
+            float = {
+              relative = 'editor',
+              row = 0.3,
+              col = 0.25,
+              width = 0.5,
+              height = 0.4,
+              border = "single",
+            },
+            horizontal = { location = "rightbelow", split_ratio = .3, },
+            vertical = { location = "rightbelow", split_ratio = .5 },
+          }
+        },
+        behavior = {
+          autoclose_on_quit = {
+            enabled = false,
+            confirm = true,
+          },
+          close_on_exit = true,
+          auto_insert = true,
+        },
+      })
+    end,
+  },
+}
+
+return plugins
+
